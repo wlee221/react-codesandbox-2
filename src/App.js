4064c8be9ada217cec1ca1ee2ceabb37c014715b
William Lee <43682783+wlee221@users.noreply.github.com>
2025-09-22 16:39:56 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox 2fddgf</h1>
+      <h2>test56</h2>
+    </div>
+  );
+}
