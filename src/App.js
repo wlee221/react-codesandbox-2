9384bd2db3c6b24f807a58f3d206e6699a747b64
William Lee <43682783+wlee221@users.noreply.github.com>
2025-09-22 17:13:57 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox 2</h1>
+      <h2>Branch: main</h2>
       <h2>This is being used to serve app d1tqbntw3gl04z</h2>
     </div>
   );
